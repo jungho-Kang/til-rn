afdf42eaecc12e0e8a8c113447cd4c7c0c11a680
jungho-Kang <rkdwjdgh08@gmail.com>
2025-02-26 14:36:24 +0900

    [docs] release 테스트하기

--- App.tsx
+++ App.tsx
@@ -11,7 +11,7 @@ import SplashScreen from 'react-native-splash-screen';
 import WebView from 'react-native-webview';
 
 const App = (): JSX.Element => {
-  const webViewUrl = 'https://app-fish-y3pa.vercel.app';
+  const webViewUrl = 'http://112.222.157.157:5222/';
 
   // back 키 처리
   useEffect(() => {
